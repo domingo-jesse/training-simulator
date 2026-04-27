a4ca0123cba1cd7adbd4565f0a69ac39c5f967ae
domingo-jesse <39747952+domingo-jesse@users.noreply.github.com>
2026-04-27 10:59:00 -0700

    Reduce top padding above page headers

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ st.markdown(
 
 /* MAIN PAGE CONTAINER */
 section.main > div {
-    padding-top: 0.25rem;
+    padding-top: 0rem;
     padding-bottom: 0rem;
     padding-left: 2rem;
     padding-right: 2rem;
@@ -90,13 +90,13 @@ section.main > div {
 
 /* NEW STREAMLIT CLASS (important) */
 .stAppViewBlockContainer {
-    padding-top: 0.25rem;
+    padding-top: 0rem;
     padding-bottom: 0rem;
 }
 
 /* REMOVE EXTRA TOP SPACE */
 .block-container {
-    padding-top: 0.25rem;
+    padding-top: 0rem;
     padding-bottom: 0rem;
     margin-top: 0rem;
 }
